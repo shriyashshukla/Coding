d159a6f6ccdfbc0f87317674d5d5f28a59926b33
shriyashshukla <harishukla797@gmail.com>
2023-08-15 16:33:46 +0530

    main

---
+++ coding/test/index.js
@@ -0,0 +1,17 @@
+console.log("helooo ram");
+
+function getarea(leghth,width){
+    return  area =leghth*width;
+
+}
+ function getperimeter (length,width){
+    let perimeter ;
+    console.log(`the length is ${length}`);
+ }
+
+ // area of a circle
+ const radius=50;
+ const pi=3.1428697;
+ const circleradius =(radius *pi);
+ console.log(circleradius);
+ 
