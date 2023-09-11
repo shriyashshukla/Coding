4c2adcebb20ef1f9a79f536b216a3a2fd266bbf6
shriyash shukla <139564061+shriyashshukla@users.noreply.github.com>
2023-09-11 12:32:28 +0530

    changes

--- coding/clone/imdb-fe/src/components/header/Header.js
+++ coding/clone/imdb-fe/src/components/header/Header.js
@@ -1,9 +1,9 @@
-import React from "react";
-import"./Header.css"
-import { Link } from "react-router-dom";
+import React from "react"
+import "./Header.css"
+import { Link } from "react-router-dom"
 
-const Header =( )=>{
-    return(
+const Header = () => {
+    return (
         <div className="header">
             <div className="headerLeft">
                 <Link to="/"><img className="header__icon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/IMDB_Logo_2016.svg/2560px-IMDB_Logo_2016.svg.png" /></Link>
